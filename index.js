0a5a4ecb97034c9ea50aab6e8f0e3d78b931ee54
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-31 03:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayTo(holiday, name)
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, Number)
+function holidayCountdown(holiday, days)
 {
   return `It's ${days} days until ${holiday}!`
 }
